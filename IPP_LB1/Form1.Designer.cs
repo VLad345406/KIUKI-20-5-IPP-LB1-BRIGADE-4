3c4f0e382262f6daa5f0f938fb9d07dd498fa1e8
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2023-04-24 21:34:48 +0300

    Realize start and stop both thread in console

--- IPP_LB1/Form1.Designer.cs
+++ IPP_LB1/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace IPP_LB1
+﻿using System.Windows.Forms;
+
+namespace IPP_LB1
 {
     partial class Form1
     {
@@ -29,12 +31,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonStartFirstThread = new System.Windows.Forms.Button();
+            this.buttonStartSecondThread = new System.Windows.Forms.Button();
+            this.groupBoxValue1 = new System.Windows.Forms.GroupBox();
+            this.labelTime1 = new System.Windows.Forms.Label();
+            this.labelValue1 = new System.Windows.Forms.Label();
+            this.labelStatus1 = new System.Windows.Forms.Label();
+            this.groupBoxValue2 = new System.Windows.Forms.GroupBox();
+            this.labelTime2 = new System.Windows.Forms.Label();
+            this.labelValue2 = new System.Windows.Forms.Label();
+            this.labelStatus2 = new System.Windows.Forms.Label();
+            this.buttonUpPriorityThread1 = new System.Windows.Forms.Button();
+            this.buttonUpPriorityThread2 = new System.Windows.Forms.Button();
+            this.buttonDownPriorityThread1 = new System.Windows.Forms.Button();
+            this.buttonDownPriorityThread2 = new System.Windows.Forms.Button();
+            this.groupBoxValue1.SuspendLayout();
+            this.groupBoxValue2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStartFirstThread
+            // 
+            this.buttonStartFirstThread.Location = new System.Drawing.Point(12, 39);
+            this.buttonStartFirstThread.Name = "buttonStartFirstThread";
+            this.buttonStartFirstThread.Size = new System.Drawing.Size(210, 61);
+            this.buttonStartFirstThread.TabIndex = 0;
+            this.buttonStartFirstThread.Text = "Запустити перший потік";
+            this.buttonStartFirstThread.UseVisualStyleBackColor = true;
+            this.buttonStartFirstThread.Click += new System.EventHandler(this.buttonStartFirstThread_Click);
+            // 
+            // buttonStartSecondThread
+            // 
+            this.buttonStartSecondThread.Location = new System.Drawing.Point(230, 39);
+            this.buttonStartSecondThread.Name = "buttonStartSecondThread";
+            this.buttonStartSecondThread.Size = new System.Drawing.Size(209, 61);
+            this.buttonStartSecondThread.TabIndex = 1;
+            this.buttonStartSecondThread.Text = "Запустити другий потік";
+            this.buttonStartSecondThread.UseVisualStyleBackColor = true;
+            this.buttonStartSecondThread.Click += new System.EventHandler(this.buttonStartSecondThread_Click);
+            // 
+            // groupBoxValue1
+            // 
+            this.groupBoxValue1.Controls.Add(this.labelTime1);
+            this.groupBoxValue1.Controls.Add(this.labelValue1);
+            this.groupBoxValue1.Controls.Add(this.labelStatus1);
+            this.groupBoxValue1.Location = new System.Drawing.Point(12, 240);
+            this.groupBoxValue1.Name = "groupBoxValue1";
+            this.groupBoxValue1.Size = new System.Drawing.Size(191, 202);
+            this.groupBoxValue1.TabIndex = 6;
+            this.groupBoxValue1.TabStop = false;
+            this.groupBoxValue1.Text = "Value thread 1";
+            // 
+            // labelTime1
+            // 
+            this.labelTime1.Location = new System.Drawing.Point(6, 95);
+            this.labelTime1.Name = "labelTime1";
+            this.labelTime1.Size = new System.Drawing.Size(159, 29);
+            this.labelTime1.TabIndex = 2;
+            this.labelTime1.Text = "Spent time: 0";
+            // 
+            // labelValue1
+            // 
+            this.labelValue1.Location = new System.Drawing.Point(6, 66);
+            this.labelValue1.Name = "labelValue1";
+            this.labelValue1.Size = new System.Drawing.Size(159, 29);
+            this.labelValue1.TabIndex = 1;
+            this.labelValue1.Text = "Current value: 0";
+            // 
+            // labelStatus1
+            // 
+            this.labelStatus1.Location = new System.Drawing.Point(6, 37);
+            this.labelStatus1.Name = "labelStatus1";
+            this.labelStatus1.Size = new System.Drawing.Size(159, 29);
+            this.labelStatus1.TabIndex = 0;
+            this.labelStatus1.Text = "Status: inactive";
+            // 
+            // groupBoxValue2
+            // 
+            this.groupBoxValue2.Controls.Add(this.labelTime2);
+            this.groupBoxValue2.Controls.Add(this.labelValue2);
+            this.groupBoxValue2.Controls.Add(this.labelStatus2);
+            this.groupBoxValue2.Location = new System.Drawing.Point(248, 240);
+            this.groupBoxValue2.Name = "groupBoxValue2";
+            this.groupBoxValue2.Size = new System.Drawing.Size(191, 202);
+            this.groupBoxValue2.TabIndex = 7;
+            this.groupBoxValue2.TabStop = false;
+            this.groupBoxValue2.Text = "Value thread 2";
+            // 
+            // labelTime2
+            // 
+            this.labelTime2.Location = new System.Drawing.Point(6, 95);
+            this.labelTime2.Name = "labelTime2";
+            this.labelTime2.Size = new System.Drawing.Size(159, 29);
+            this.labelTime2.TabIndex = 3;
+            this.labelTime2.Text = "Spent time: 0";
+            // 
+            // labelValue2
+            // 
+            this.labelValue2.Location = new System.Drawing.Point(6, 66);
+            this.labelValue2.Name = "labelValue2";
+            this.labelValue2.Size = new System.Drawing.Size(159, 29);
+            this.labelValue2.TabIndex = 2;
+            this.labelValue2.Text = "Current value: 0";
+            // 
+            // labelStatus2
+            // 
+            this.labelStatus2.Location = new System.Drawing.Point(6, 37);
+            this.labelStatus2.Name = "labelStatus2";
+            this.labelStatus2.Size = new System.Drawing.Size(159, 29);
+            this.labelStatus2.TabIndex = 1;
+            this.labelStatus2.Text = "Status: inactive";
+            // 
+            // buttonUpPriorityThread1
+            // 
+            this.buttonUpPriorityThread1.Location = new System.Drawing.Point(12, 106);
+            this.buttonUpPriorityThread1.Name = "buttonUpPriorityThread1";
+            this.buttonUpPriorityThread1.Size = new System.Drawing.Size(210, 61);
+            this.buttonUpPriorityThread1.TabIndex = 2;
+            this.buttonUpPriorityThread1.Text = "Збільшити пріорітет першого потоку";
+            this.buttonUpPriorityThread1.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpPriorityThread2
+            // 
+            this.buttonUpPriorityThread2.Location = new System.Drawing.Point(230, 106);
+            this.buttonUpPriorityThread2.Name = "buttonUpPriorityThread2";
+            this.buttonUpPriorityThread2.Size = new System.Drawing.Size(210, 61);
+            this.buttonUpPriorityThread2.TabIndex = 3;
+            this.buttonUpPriorityThread2.Text = "Збільшити пріорітет другого потоку";
+            this.buttonUpPriorityThread2.UseVisualStyleBackColor = true;
+            // 
+            // buttonDownPriorityThread1
+            // 
+            this.buttonDownPriorityThread1.Location = new System.Drawing.Point(12, 173);
+            this.buttonDownPriorityThread1.Name = "buttonDownPriorityThread1";
+            this.buttonDownPriorityThread1.Size = new System.Drawing.Size(210, 61);
+            this.buttonDownPriorityThread1.TabIndex = 4;
+            this.buttonDownPriorityThread1.Text = "Зменшити пріорітет першого потоку";
+            this.buttonDownPriorityThread1.UseVisualStyleBackColor = true;
+            // 
+            // buttonDownPriorityThread2
+            // 
+            this.buttonDownPriorityThread2.Location = new System.Drawing.Point(230, 173);
+            this.buttonDownPriorityThread2.Name = "buttonDownPriorityThread2";
+            this.buttonDownPriorityThread2.Size = new System.Drawing.Size(210, 61);
+            this.buttonDownPriorityThread2.TabIndex = 5;
+            this.buttonDownPriorityThread2.Text = "Зменшити пріорітет другого потоку";
+            this.buttonDownPriorityThread2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(451, 454);
+            this.Controls.Add(this.buttonDownPriorityThread2);
+            this.Controls.Add(this.buttonDownPriorityThread1);
+            this.Controls.Add(this.buttonUpPriorityThread2);
+            this.Controls.Add(this.buttonUpPriorityThread1);
+            this.Controls.Add(this.groupBoxValue2);
+            this.Controls.Add(this.groupBoxValue1);
+            this.Controls.Add(this.buttonStartSecondThread);
+            this.Controls.Add(this.buttonStartFirstThread);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.groupBoxValue1.ResumeLayout(false);
+            this.groupBoxValue2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
-        #endregion
+        private System.Windows.Forms.Label labelValue1;
+        private System.Windows.Forms.Label labelValue2;
+        private System.Windows.Forms.Label labelTime1;
+        private System.Windows.Forms.Label labelTime2;
+
+        private System.Windows.Forms.Label labelStatus2;
+
+        private System.Windows.Forms.Label labelStatus1;
+
+        private System.Windows.Forms.GroupBox groupBoxValue1;
+        private System.Windows.Forms.GroupBox groupBoxValue2;
+
+        private System.Windows.Forms.Button buttonStartSecondThread;
+
+        private System.Windows.Forms.Button buttonStartFirstThread;
+        private Button buttonUpPriorityThread1;
+        private Button buttonUpPriorityThread2;
+        private Button buttonDownPriorityThread1;
+        private Button buttonDownPriorityThread2;
     }
+    #endregion
 }
--- IPP_LB1/Form1.cs
+++ IPP_LB1/Form1.cs
@@ -1,20 +1,125 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace IPP_LB1
 {
     public partial class Form1 : Form
     {
+        private static int _c1 = 1;
+        
+        private static void FuncThread1()
+        {
+            _c1 = 1;
+            while (true)
+            {
+                Console.WriteLine(_c1 + @" (First thread)");
+                Thread.Sleep(1);
+                _c1++;
+                if (_c1 != 100000) continue;
+                Thread.Sleep(10000);
+                _c1 = 1;
+            }
+        }
+
+        
+
+        private static void FuncThread2()
+        {
+            var c = 1;
+            while (true)
+            {
+                Console.WriteLine(new string(' ', 100) + c + @" (Second thread)");
+                Thread.Sleep(1);
+                c++;
+                if (c != 100000) continue;
+                Thread.Sleep(10000);
+                c = 1;
+            }
+        }
+        
+        
+
+        private static ThreadStart _threadStart1 = new ThreadStart(FuncThread1);
+        private static Thread _thread1 = new Thread(_threadStart1);
+        
+        private static void FuncStatusThread1()
+        {
+
+            //_thread1.Priority = ;
+        }
+        
+        private static ThreadStart _threadStartStatus1 = new ThreadStart(FuncStatusThread1);
+        private Thread _threadStatus1 = new Thread(_threadStartStatus1);
+        
+        private static ThreadStart _threadStart2 = new ThreadStart(FuncThread2);
+        private Thread _thread2 = new Thread(_threadStart2);
+        
+        private static void FuncStatusThread2()
+        {
+            
+        }
+        
+        private static ThreadStart _threadStartStatus2 = new ThreadStart(FuncStatusThread2);
+        private Thread _threadStatus2 = new Thread(_threadStartStatus2);
+        
         public Form1()
         {
             InitializeComponent();
+            Consondeb();
+        }
+
+        private static void Consondeb()
+        {
+            if (NativeMethods.AllocConsole())
+            {
+                IntPtr stdHandle = NativeMethods.GetStdHandle(NativeMethods.STD_OUTPUT_HANDLE);
+            }
+        }
+        public abstract class NativeMethods
+        {
+            public static Int32 STD_OUTPUT_HANDLE = -11;
+
+            [System.Runtime.InteropServices.DllImportAttribute("kernel32.dll", EntryPoint = "GetStdHandle")]
+            public static extern System.IntPtr GetStdHandle(Int32 nStdHandle);
+
+            [System.Runtime.InteropServices.DllImportAttribute("kernel32.dll", EntryPoint = "AllocConsole")]
+            [return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+            public static extern bool AllocConsole();
+        }
+
+        private void buttonStartFirstThread_Click(object sender, EventArgs e)
+        {
+            if (!_thread1.IsAlive)
+            {
+                buttonStartFirstThread.Text = @"Зупинити перший потік";
+                labelStatus1.Text = @"Status: active";
+                _thread1.Start();
+            }
+            else
+            {
+                buttonStartFirstThread.Text = @"Запустити перший потік";
+                labelStatus1.Text = @"Status: inactive";
+                _thread1.Abort();
+                _thread1 = new Thread(_threadStart1);
+            }
+        }
+
+        private void buttonStartSecondThread_Click(object sender, EventArgs e)
+        {
+            if (!_thread2.IsAlive)
+            {
+                buttonStartSecondThread.Text = @"Зупинити другий потік";
+                labelStatus2.Text = @"Status: active";
+                _thread2.Start();
+            }
+            else
+            {
+                buttonStartSecondThread.Text = @"Запустити другий потік";
+                labelStatus2.Text = @"Status: inactive";
+                _thread2.Abort();
+                _thread2 = new Thread(_threadStart2);
+            }
         }
     }
 }
